d0c6c12e10b0838ab711059d76664ba6eb85310d
Rasmus Mikkelstrup <201804618@post.au.dk>
2020-09-02 12:51:44 +0200

    Solved: After the player red has taken their turn, it shall be player blue's turn. No fake code added

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -30,18 +30,24 @@ import hotciv.framework.*;
 */
 
 public class GameImpl implements Game {
+  Player currentPlayer = Player.RED;
+
   public Tile getTileAt( Position p ) { return null; }
   public Unit getUnitAt( Position p ) { return null; }
   public City getCityAt( Position p ) { return null; }
   public Player getPlayerInTurn() {
-    return Player.RED;
+    return currentPlayer;
   }
   public Player getWinner() { return null; }
   public int getAge() { return 0; }
   public boolean moveUnit( Position from, Position to ) {
     return false;
   }
-  public void endOfTurn() {}
+  public void endOfTurn() {
+    if (currentPlayer == Player.RED) {
+      currentPlayer = Player.BLUE;
+    }
+  }
   public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
   public void changeProductionInCityAt( Position p, String unitType ) {}
   public void performUnitActionAt( Position p ) {}
