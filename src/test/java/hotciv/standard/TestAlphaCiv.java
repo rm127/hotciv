cd331149d3e2e63ccc72e051529edf2e7bb719a1
Rasmus Mikkelstrup <201804618@post.au.dk>
2020-09-11 12:38:27 +0200

    fuckup: Added missing test 🙃

--- src/test/java/hotciv/standard/TestAlphaCiv.java
+++ src/test/java/hotciv/standard/TestAlphaCiv.java
@@ -330,12 +330,18 @@ public class TestAlphaCiv {
     assertThat(tile.getTypeString(), is(HILLS));
   }
 
-  // Unit cannot be moved to an invalid tile (ocean and mountain)
+  // Unit cannot be moved to a mountain tile
   @Test
   void movingUnitToMountainIsNotPossible() {
     // change turn to player blue to move blue unit at pos (2,3)
     game.endOfTurn();
     assertThat(game.moveUnit(new Position(3, 2), new Position(2, 2)), is(false));
+  }
+
+  // Unit cannot be moved to an ocean tile
+  @Test
+  void movingUnitToOceanIsNotPossible() {
+    assertThat(game.moveUnit(new Position(2, 0), new Position(1, 0)), is(false));
   }
 
 
