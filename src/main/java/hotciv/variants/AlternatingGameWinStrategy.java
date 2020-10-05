be8a868af071f9f2b5a828c67b1d85cbfb246307
Rasmus Mikkelstrup <201804618@post.au.dk>
2020-10-05 17:18:56 +0200

    Updated to make sure the correct state is indeed active

--- src/main/java/hotciv/variants/AlternatingGameWinStrategy.java
+++ src/main/java/hotciv/variants/AlternatingGameWinStrategy.java
@@ -5,28 +5,37 @@ import hotciv.common.GameWinStrategy;
 import hotciv.framework.Player;
 
 public class AlternatingGameWinStrategy implements GameWinStrategy {
+    GameWinStrategy beforeRound21Strategy;
     GameWinStrategy afterRound20Strategy;
     GameWinStrategy currentState;
 
     private int currentRound = 1;
 
     public AlternatingGameWinStrategy(GameWinStrategy beforeRound21Strategy, GameWinStrategy afterRound20Strategy) {
+        this.beforeRound21Strategy = beforeRound21Strategy;
         this.afterRound20Strategy = afterRound20Strategy;
         this.currentState = beforeRound21Strategy;
     }
 
     public Player getWinner(GameImpl game) {
+        checkState();
         return currentState.getWinner(game);
     }
 
     public void incrementBattleWon(Player p) {
+        checkState();
         currentState.incrementBattleWon(p);
     }
 
     public void incrementRoundNumber() {
         currentRound++;
+    }
+
+    private void checkState() {
         if (currentRound > 20) {
             currentState = afterRound20Strategy;
+        } else {
+            currentState = beforeRound21Strategy;
         }
     }
 }
