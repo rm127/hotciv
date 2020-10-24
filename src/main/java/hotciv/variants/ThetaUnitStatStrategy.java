67c8db6d4c47333fe142aa1a56cb6cd9aded2f51
Rasmus Mikkelstrup <201804618@post.au.dk>
2020-10-24 13:50:31 +0200

    Changed the strategies to avoid inheritance, but instead use a compositional design.

--- src/main/java/hotciv/variants/ThetaUnitStatStrategy.java
+++ src/main/java/hotciv/variants/ThetaUnitStatStrategy.java
@@ -1,15 +1,32 @@
 package hotciv.variants;
 
-public class ThetaUnitStatStrategy extends AlphaUnitStatStrategy {
+import hotciv.common.UnitStatStrategy;
+
+public class ThetaUnitStatStrategy implements UnitStatStrategy {
     private static final String CARAVAN = "caravan";
+    private static UnitStatStrategy baseStrategy;
 
     public ThetaUnitStatStrategy() {
-        super();
+        baseStrategy = new AlphaUnitStatStrategy();
+    }
 
-        // adding Caravan
-        defenseStrengthMap.put(CARAVAN, 4);
-        attackingStrengthMap.put(CARAVAN, 1);
-        moveCountMap.put(CARAVAN, 2);
-        costMap.put(CARAVAN, 30);
+    public int getDefenseStrength(String unitType) {
+        if (unitType.equals(CARAVAN)) return 4;
+        return baseStrategy.getDefenseStrength(unitType);
+    }
+
+    public int getAttackingStrength(String unitType) {
+        if (unitType.equals(CARAVAN)) return 1;
+        return baseStrategy.getAttackingStrength(unitType);
+    }
+
+    public int getMoveCount(String unitType) {
+        if (unitType.equals(CARAVAN)) return 2;
+        return baseStrategy.getMoveCount(unitType);
+    }
+
+    public int getCost(String unitType) {
+        if (unitType.equals(CARAVAN)) return 30;
+        return baseStrategy.getCost(unitType);
     }
 }
